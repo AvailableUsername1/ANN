ab821f7e7f42e7620551c8752730d779ec94c452
AvailableUsername1 <73890180+AvailableUsername1@users.noreply.github.com>
2022-03-04 13:47:52 +0200

    implemented the feedforward part

--- network.py
+++ network.py
@@ -8,5 +8,29 @@ class Network(object):
         of layers and the value of individual elements represent the
         number of neurons. 
         """
+        self.numlayers = len(layers)
         self.weights = [np.random.randn(j, k) for j, k in zip(layers[1:], layers[0:-1])]
         self.biases = [np.random.randn(j, 1) for j in layers[1:]]
+
+    def feedforward(self, batch):
+        """
+        Performing forward propagation on the given batch. Saving the
+        weighted inputs of each layer in a list "zlist" and activations
+        in a list "alist" that is going to be used in backpropagation.
+        """
+
+        a = batch
+        zlist = []
+        alist = [batch]
+
+        for step in range(self.numlayers - 1):
+            z = np.dot(self.weights[step], a) + self.biases[step]
+            zlist.append(z)
+            a = sigmoid(z)
+            alist.append(a)
+        
+        return zlist, alist
+
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
