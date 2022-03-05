43a097b0340d56821f87ad53989c63135c91adbf
AvailableUsername1 <73890180+AvailableUsername1@users.noreply.github.com>
2022-03-05 14:30:07 +0200

    added backprop functionality
    calculating derivatives of weight and bias matrices using a batch of training examples

--- network.py
+++ network.py
@@ -31,6 +31,40 @@ class Network(object):
         
         return zlist, alist
 
+    def backprop(self, batchsize, ybatch, zlist, alist):
+        """
+        Calculating the gradients of weights and biases with respect to
+        the cost in a batch. Returning weightders - list containing all
+        weight derivatives starting with the end - and biasders - list
+        containing all bias derivatices starting with the end. All of them
+        are divided by the number of examples in a batch
+        """
+        errors = []
+        weightders = []
+        biasders = []
+
+        error = ybatch - alist[-1]
+        errors.append(error)
+        weightder = np.dot(error, np.transpose(alist[-2]))
+        weightders.append(weightder / batchsize)
+        biasder = np.sum(error, axis=1, keepdims=True) / batchsize
+        biasders.append(biasder)
+
+        for i in range(2, self.numlayers):
+            error = np.multiply(np.dot(np.transpose(self.weights[-i + 1]), error), sigmoidprime(zlist[-i]))
+            errors.append(error)
+            weightder = np.dot(error, np.transpose(alist[-i-1]))
+            weightders.append(weightder / batchsize)
+            biasder = np.sum(error, axis=1, keepdims=True) / batchsize
+            biasders.append(biasder)
+
+        return weightders, biasders
+
+
+
 
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
+
+def sigmoidprime(z):
+    return sigmoid(z) * (1 - sigmoid(z))
